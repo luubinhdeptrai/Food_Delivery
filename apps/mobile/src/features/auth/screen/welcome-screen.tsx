e1edead46df6eae884600ec11d1cd019cffde48d
luubinhdeptrai <luubinh.chuyenlongan@gmail.com>
2026-05-21 11:46:57 +0700

    feat: update welcome screen image to use JPEG format

--- apps/mobile/src/features/auth/screen/welcome-screen.tsx
+++ apps/mobile/src/features/auth/screen/welcome-screen.tsx
@@ -105,7 +105,7 @@ export function WelcomeScreen({
             className="relative w-full overflow-hidden"
           >
             <Image
-              source={require("../../../../assets/images/welcome-hero.png")}
+              source={require("../../../../assets/images/welcome-hero.jpg")}
               style={{ width: "100%", height: "100%" }}
               contentFit="cover"
             />
