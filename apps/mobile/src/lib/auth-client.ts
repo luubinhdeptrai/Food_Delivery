b7148939c01f79ea722104aac119957a4af8eccc
luubinhdeptrai <luubinh.chuyenlongan@gmail.com>
2026-05-29 10:40:48 +0700

    feat: remove PostHog analytics integration and update environment variables

--- apps/mobile/src/lib/auth-client.ts
+++ apps/mobile/src/lib/auth-client.ts
@@ -4,11 +4,13 @@ import { phoneNumberClient } from "better-auth/client/plugins";
 import * as SecureStore from "expo-secure-store";
 import Constants from "expo-constants";
 
-// Get the local IP address for the backend when running in development
 const getBaseUrl = () => {
+  if (process.env.EXPO_PUBLIC_API_URL) {
+    return process.env.EXPO_PUBLIC_API_URL.replace(/\/+$/, "");
+  }
   const debuggerHost = Constants.expoConfig?.hostUri;
   const localhost = debuggerHost?.split(":")[0] || "localhost";
-  return `http://${localhost}:3000`;
+  return `http://${localhost}:3000`.replace(/\/+$/, "");
 };
 
 export const authClient = createAuthClient({
